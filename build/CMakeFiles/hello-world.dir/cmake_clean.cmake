a62e4482462e5cfdf34c53a33c2a4ea1cface3fc
anjiyuan <anjiyuan@yahoo.com>
2021-02-06 23:43:41 +1000

    first commit

---
+++ build/CMakeFiles/hello-world.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello-world.dir/hello-world.cpp.o"
+  "Debug/hello-world"
+  "Debug/hello-world.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello-world.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
